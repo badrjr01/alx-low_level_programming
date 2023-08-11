fb25a196529c2d9033fab500998dfc2b2412bea7
badrjr01 <badrjouari0@gmail.com>
2023-08-10 17:48:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * description: A C program that prints with put function 
+ *
+ *return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
