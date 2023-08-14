1c001a35a78ec0a55d5dfcbedca7fba5455daab9
badrjr01 <badrjouari0@gmail.com>
2023-08-14 07:50:21 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * description: A C program that prints with put function
+ * Description: A C program that prints with put function
  *
- *return: Alawys 0 (success)
+ * Return: Alawys 0 (success)
 */
 
 int main(void)
